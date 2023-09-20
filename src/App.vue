0f7fa9ef39e9216f0c4378a40588eac42404a932
Evgeniya <evgeniya@ultimatemovers.net>
2023-09-20 16:37:37 +0200

    deleted unused assetns and added tailwind to project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="bg-red-200">hello there</h1>
+  <p class="border-b-2 border-fuchsia-600 hello">can you see me?</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
