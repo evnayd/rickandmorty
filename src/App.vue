6c756099b206aa68e6af63ff522245de8aa0a904
Evgeniya <evgeniya@ultimatemovers.net>
2023-09-23 17:37:27 +0200

    added infinitive scroll logic

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,83 @@
 <template>
-  <character-gallery :characters="characters"></character-gallery>
+  <div ref="scrollContainer" class="infinite-scroll-container">
+    <character-gallery :characters="computedCharacters"></character-gallery>
+  </div>
 </template>
 
 <script>
 import CharacterGallery from "./components/CharacterGallery.vue";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, onUnmounted, computed } from "vue";
 
 export default {
   name: "App",
   components: { CharacterGallery },
   setup() {
     const characters = ref([]);
+    const pageNumber = ref(1);
+    const isLoading = ref(false);
+    const bottom = ref(false);
 
     // Function to fetch data from the server
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://rickandmortyapi.com/api/character"
+          `https://rickandmortyapi.com/api/character/?page=${pageNumber.value}`
         );
         const result = await response.json();
-        console.log("result", result);
 
-        characters.value = result.results;
-        console.log("characters.value", result.results);
+        if (result.results) {
+          characters.value = characters.value.concat(result.results);
+
+          pageNumber.value++;
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        isLoading.value = false;
+      }
+    };
+
+    const doScroll = (event) => {
+      const scrollHeight = event.target.scrollHeight;
+      const scrollTop = event.target.scrollTop;
+      const clientHeight = event.target.clientHeight;
+
+      if (scrollTop + clientHeight >= scrollHeight) {
+        console.log("Yay!");
+        bottom.value = true;
+        fetchData();
+      } else {
+        bottom.value = false;
       }
     };
 
     onMounted(() => {
       fetchData();
+      const container = document.querySelector(".infinite-scroll-container");
+      container.addEventListener("scroll", doScroll);
     });
 
+    // Removing the scroll event listener when the component is unmounted
+    onUnmounted(() => {
+      const container = document.querySelector(".infinite-scroll-container");
+      container.addEventListener("scroll", doScroll);
+    });
+
+    const computedCharacters = computed(() => characters.value);
+    console.log(" computedCharacters", computedCharacters);
+
     return {
-      characters,
+      computedCharacters,
     };
   },
 };
 </script>
+
+<style scoped>
+.infinite-scroll-container {
+  height: 2500px;
+  overflow-y: scroll;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+</style>
